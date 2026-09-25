ca3d8f8cd84b884ff408f3f1d4aafd8a4637b8bf
Nicolas Javier Agostino <nico.agostino@hotmail.com>
2026-09-25 15:14:55 -0300

    style: update css

--- src/js/components/renderCards.js
+++ src/js/components/renderCards.js
@@ -5,10 +5,14 @@ export const renderCard = (pokemon) => {
         <div class="card col" style="width: 18rem;">
             <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="...">
             <div class="card-body">
-                <span>#${pokemon.id}</span>
-                <h5 class="card-title text-capitalize">${pokemon.name}</h5>
+                <span class="card-id">#${String(pokemon.id).padStart(3,0)}</span>
+                <h5 class="card-title text-capitalize mt-3 mb-3">${pokemon.name}</h5>
                 <div>
-                    ${pokemon.types.map( p => `<span class="text-capitalize">${p.type.name}</span>`).join(" / ")}
+                    ${pokemon.types.map( p => `<span class="type-pokemon type-${p.type.name}  text-capitalize">${p.type.name}</span>`).join(" / ")}
+                </div>
+                <div class="card-desc">
+                    <p>Altura: ${pokemon.height/ 10}m</p>
+                    <p>Peso: ${pokemon.weight/ 10}kg</p>
                 </div>
             </div>
         </div>`;
